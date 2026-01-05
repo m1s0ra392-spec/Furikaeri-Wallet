1e84d82a74f614ec9609e1f248276d0426a5734c
守田ちはる <m1.s0.ra392@gmail.com>
2026-01-06 04:45:24 +0900

    ログイン画面遷移

--- furikaeri-wallet_project/urls.py
+++ furikaeri-wallet_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    #path("accounts/", include("django.contrib.auth.urls")), #仮
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),  #Djano標準のログイン・ログアウト
+    path("users/", include("users.urls")),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+
 from .import views
 
 
@@ -7,7 +7,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name = "users/login.html"), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+
 ]
