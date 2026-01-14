b16697e1a04a94eda150d40bfaf3689d50103c90
守田ちはる <m1.s0.ra392@gmail.com>
2026-01-14 13:08:18 +0900

    記録一覧表示・編集・削除

--- records/urls.py
+++ records/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "records"
 
 urlpatterns = [
-    path("new/", views.record_create, name="record_create"),
-    path("", views.record_list, name="list"),
+    path("new/", views.record_create, name="record_create"), #記録入力画面
+    path("", views.record_list, name="list"), #記録一覧（カレンダーの上から）
+    path("<int:pk>/edit/", views.record_update, name="record_update"),#記録編集
+    path("<int:pk>/delete/", views.record_delete, name="record_delete"),#記録削除
 ]
