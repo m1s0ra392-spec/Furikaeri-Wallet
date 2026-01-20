0c6048c324b99a80b414520a55962df4e7f107e3
守田ちはる <m1.s0.ra392@gmail.com>
2026-01-21 04:47:17 +0900

    カレンダー編集後の遷移先エラーの修正

--- records/urls.py
+++ records/urls.py
@@ -5,7 +5,7 @@ app_name = "records"
 
 urlpatterns = [
     path("new/", views.record_create, name="record_create"), #記録入力画面
-    path("", views.record_list, name="list"), #記録一覧（カレンダーの上から）
+    path("", views.record_list, name="record_list"), #記録一覧（カレンダーの上から）
     path("<int:pk>/edit/", views.record_update, name="record_update"),#記録編集
     path("<int:pk>/delete/", views.record_delete, name="record_delete"),#記録削除
 ]
