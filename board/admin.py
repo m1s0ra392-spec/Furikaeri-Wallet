899cce8fabb8aee740a42cfcab70771f3212cd8f
守田ちはる <m1.s0.ra392@gmail.com>
2026-02-07 07:20:56 +0900

    adminにて動作確認

--- board/admin.py
+++ board/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
-from .models import Topic
-from .models import Comment
+from .models import Topic, Comment, Tag, TopicTag
+
 # Register your models here.
+
+class TopicTagInline(admin.TabularInline):
+    model = TopicTag
+    extra = 1
+    autocomplete_fields = ("tag",)
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "board_category", "status", "user", "created_at")
     list_filter = ("board_category", "status")
     search_fields = ("title", "text")
+    inlines = [TopicTagInline]
     
 admin.site.register(Comment)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_active", "merged_to_tag", "created_at", "updated_at")
+    search_fields = ("name",)
+    list_filter = ("is_active",)
+    autocomplete_fields = ("merged_to_tag",)
+
+
+@admin.register(TopicTag)
+class TopicTagAdmin(admin.ModelAdmin):
+    list_display = ("id", "topic", "tag", "created_at", "updated_at")
+    search_fields = ("topic__title", "tag__name")
+    autocomplete_fields = ("topic", "tag")
