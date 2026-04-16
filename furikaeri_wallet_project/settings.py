3bf4e8d62be1465a4ea0b44aa4671bf5eb2b0f0e
守田ちはる <m1.s0.ra392@gmail.com>
2026-04-16 19:57:29 +0900

    メールアドレス送信元名前変更

--- furikaeri_wallet_project/settings.py
+++ furikaeri_wallet_project/settings.py
@@ -110,7 +110,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', '')
+DEFAULT_FROM_EMAIL = 'ふりかえり財布 <' + os.environ.get('EMAIL_HOST_USER', '') + '>'
 EMAIL_CHARSET = 'utf-8'
 
     
